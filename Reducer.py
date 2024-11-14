6962a327196ab8ffe77fefe3df5486ce5de2626a
jackeyf <yangfei@umich.edu>
2024-11-13 22:45:29 -0800

    modify our reducer

--- Reducer.py
+++ Reducer.py
@@ -12,7 +12,7 @@ for line in sys.stdin:
     line = line.strip()
     key, word, count = line.split('\t', 2)
     count = int(count)
-    global_counter[word] += count
+    global_counter[word] += (count*2)
 
 sorted_words = sorted(global_counter.items(), key=lambda x: x[1], reverse=True)
 for word, count in sorted_words[:num]:
